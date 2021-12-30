5c021593c1d509af5da898d6f57b58185aaad6d9
João FIlipi Britto <jf.britto@hotmail.com>
2021-12-29 23:59:35 -0300

    meta fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,16 @@
 
 @section('meta_tags')
 <link rel="icon" href="/img/favicon.svg" type="image/svg">
-<img class="mb-5" src="/img/favicon.svg" alt="" />
-<h1 class="mb-0">Teste Superlógica</h1>
+
+<meta property="og:url" content="{{env('APP_URL')}}" />
+<meta property="type" content="website" />
+<meta property="og:title" content="Superlógica">
+<meta property="og:description" content="Teste">
+<meta property="og:image" content="/img/logo.jpg">
+<meta property="og:locale" content="pt_BR">
+<meta property="og:image:type" content="image/jpg">
+<meta property="og:image:width" content="640">
+<meta property="og:image:height" content="480">
 @stop
 
 @section('title', 'Superlógica')
